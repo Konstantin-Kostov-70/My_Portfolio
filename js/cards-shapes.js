4188ad3ae16753c2250b91d9e34a8a32fc67ef37
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-06-24 00:34:01 +0200

    Refactoring code

--- js/cards-shapes.js
+++ js/cards-shapes.js
@@ -10,8 +10,8 @@ function hideDisplayCards() {
         event.preventDefault();
         if (btn.textContent === "view all ~~>") {
             btn.textContent = "hide ~~>";
-            cards.forEach(card => card.style.display = "block");
-            shapes.forEach(shape => shape.style.display = "block");
+            cards.forEach(card => card.style.display = "flex");
+            shapes.forEach(shape => shape.style.display = "flex");
             btn.setAttribute("btn-clicked", "true");
 
         } else if (btn.textContent === "hide ~~>") {
@@ -37,16 +37,16 @@ function hideDisplayCards() {
 
             // Restore previous state if buttons were clicked
             if (firsButton.getAttribute("btn-clicked") === "true") {
-                hiddenCards.forEach(card => card.style.display = "block");
-                hiddenShapesUp.forEach(shape => shape.style.display = "block");
+                hiddenCards.forEach(card => card.style.display = "flex");
+                hiddenShapesUp.forEach(shape => shape.style.display = "flex");
                 firsButton.textContent = "hide ~~>";
             } else {
                 hiddenCards.forEach(card => card.style.display = "none");
                 hiddenShapesUp.forEach(shape => shape.style.display = "none");
             }
             if (secondButton.getAttribute("btn-clicked") === "true") {
-                hiddenCardsSmall.forEach(card => card.style.display = "block");
-                hiddenShapesDown.forEach(shape => shape.style.display = "block");
+                hiddenCardsSmall.forEach(card => card.style.display = "flex");
+                hiddenShapesDown.forEach(shape => shape.style.display = "flex");
                 secondButton.textContent = "hide ~~>";
             } else {
                 hiddenCardsSmall.forEach(card => card.style.display = "none");
@@ -56,8 +56,8 @@ function hideDisplayCards() {
             firsButton.removeEventListener("click", firsButtonHandler);
             secondButton.removeEventListener("click", secondButtonHandler);
 
-            hiddenCards.forEach(card => card.style.display = "block");
-            hiddenCardsSmall.forEach(card => card.style.display = "block");
+            hiddenCards.forEach(card => card.style.display = "flex");
+            hiddenCardsSmall.forEach(card => card.style.display = "flex");
             hiddenShapesUp.forEach(shape => shape.style.display = "none");
             hiddenShapesDown.forEach(shape => shape.style.display = "none");
         }
