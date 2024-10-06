5f7831bd2bc8cabb682e01f67a30a755a345cdba
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-06 12:17:26 +0200

    update time for alert messages

--- js/mail.js
+++ js/mail.js
@@ -21,7 +21,7 @@ function SendMail(event) {
         
         setTimeout(() => {
             formMessage.style.display = "none";
-        }, 2000);
+        }, 3000);
     }
 
     if (!fromName || !emailId || !title || !message) {
